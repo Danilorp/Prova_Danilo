e31c5adb32552913bdcecdb0522f222ff6a4cf43
Danilo Pinha <danilorpinha@hotmail.com>
2021-06-22 21:03:25 -0300

    Add project files.

---
+++ Data/Migrations/20210622232448_prova.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prova_Danilo.Data.Migrations
+{
+    public partial class prova : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
